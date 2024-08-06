074fe19cca7f4a9ede4264a8e14f49ee025650ce
Alexei Sanko <alexeisanko@gmail.com>
2024-08-06 10:04:50 +0300

    add part front

--- quiz/main/urls.py
+++ quiz/main/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import (
+    IndexView,
+    NewQuizView,
+    BankQuestionView,
+    NewsView,
+    TeamsView,
+    ApplicationsView,
+    UsersView,
+)
 
 app_name = "main"
 urlpatterns = [
     path("", view=IndexView.as_view(), name="index"),
+    path("new-quiz/", view=NewQuizView.as_view(), name="new-quiz"),
+    path("bank-question/", view=BankQuestionView.as_view(), name="bank-question"),
+    path("news/", view=NewsView.as_view(), name="news"),
+    path("teams/", view=TeamsView.as_view(), name="teams"),
+    path("applications/", view=ApplicationsView.as_view(), name="applications"),
+    path("users/", view=UsersView.as_view(), name="users"),
 ]
--- quiz/main/views.py
+++ quiz/main/views.py
@@ -3,3 +3,27 @@ from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
     template_name = "pages/index.html"
+
+
+class NewQuizView(TemplateView):
+    template_name = "pages/new-quiz.html"
+
+
+class BankQuestionView(TemplateView):
+    template_name = "pages/bank-question.html"
+
+
+class NewsView(TemplateView):
+    template_name = "pages/news.html"
+
+
+class TeamsView(TemplateView):
+    template_name = "pages/teams.html"
+
+
+class ApplicationsView(TemplateView):
+    template_name = "pages/applications.html"
+
+
+class UsersView(TemplateView):
+    template_name = "pages/users.html"
